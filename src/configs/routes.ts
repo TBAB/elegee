8ed852debb913693b9e583501c715ecfec12b5a0
赵阳 <zhaoyang@zhaoyangdeMacBook-Pro.local>
2022-10-09 01:48:50 +0800

    feat 调整路由

--- src/configs/routes.ts
+++ src/configs/routes.ts
@@ -1,11 +1,10 @@
 import { RouteRecordRaw } from "vue-router";
-import IndexPage from "../pages/IndexPage.vue";
 import GamePage from "../pages/GamePage.vue";
 
 export default [
   {
     path: "/",
-    component: IndexPage,
+    component: GamePage,
   },
   {
     path: "/game",
--- src/core/gameConfig.ts
+++ src/core/gameConfig.ts
@@ -9,56 +9,13 @@ export const defaultGameConfig: GameConfigType = {
   // 需要多少个一样块的才能合成
   composeNum: 3,
   // 动物类别数
-  typeNum: 12,
-  // 每层块数（大致）
-  levelBlockNum: 24,
-  // 边界收缩步长
-  borderStep: 1,
-  // 总层数（最小为 2）
-  levelNum: 6,
-  // 随机区块数（数组长度代表随机区数量，值表示每个随机区生产多少块）
-  randomBlocks: [8, 8],
-  // 动物数组
-  animals,
-};
-
-/**
- * 小鹅难度
- */
-export const easyGameConfig: GameConfigType = {
-  // 槽容量
-  slotNum: 7,
-  // 需要多少个一样块的才能合成
-  composeNum: 3,
-  // 动物类别数
-  typeNum: 8,
-  // 每层块数（大致）
-  levelBlockNum: 10,
-  // 边界收缩步长
-  borderStep: 1,
-  // 总层数（最小为 2）
-  levelNum: 6,
-  // 随机区块数（数组长度代表随机区数量，值表示每个随机区生产多少块）
-  randomBlocks: [4, 4],
-  // 动物数组
-  animals,
-};
-/**
- * 老鹅难度
- */
-export const yangGameConfig: GameConfigType = {
-  // 槽容量
-  slotNum: 7,
-  // 需要多少个一样块的才能合成
-  composeNum: 3,
-  // 动物类别数
   typeNum: 8,
   // 每层块数（大致）
   levelBlockNum: 16,
   // 边界收缩步长
   borderStep: 3,
   // 总层数（最小为 2）
-  levelNum: 6,
+  levelNum: 4,
   // 随机区块数（数组长度代表随机区数量，值表示每个随机区生产多少块）
   randomBlocks: [8, 8],
   // 动物数组
